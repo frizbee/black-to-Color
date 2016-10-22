339a90acb92a3fdc13ed8f43133670b1b6f6b55c
Frizbee Vivaldi <frizbee@vivaldi.net>
2016-10-22 15:18:21 +0700

    fix immigration website with frame loaded

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 /// Looking for something, whant to help improoving?
+
+$('frame').load( function() {
+    $('frame').contents().find("head").append($("<style type='text/css'>  body {-webkit-filter: grayscale(0) !important;}  </style>"));
+});

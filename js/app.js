f3ac76354d1430b1d41e701dad206b97075a0ed7
Frizbee Vivaldi <frizbee@vivaldi.net>
2016-10-22 15:37:59 +0700

    some tests

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /// Looking for something, whant to help improoving?
 
 $('frame').load( function() {
-    $('frame').contents().find("head").append($("<style type='text/css'>  body {-webkit-filter: grayscale(0) !important;}  </style>"));
+    $('frame').contents().find("head").append($("<style type='text/css'>  html, body, img {-webkit-filter: grayscale(0) !important;}  </style>"));
 });

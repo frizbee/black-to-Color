ea75a2538bae0cd2150b024bd9c2dbb5ebd1953b
Frizbee Vivaldi <frizbee@vivaldi.net>
2016-10-25 13:29:15 +0700

    add jquery force add style to body and img elements

--- js/app.js
+++ js/app.js
@@ -3,3 +3,6 @@
 $('frame').load( function() {
     $('frame').contents().find("head").append($("<style type='text/css'>  html, body, img {-webkit-filter: grayscale(0) !important;}  </style>"));
 });
+
+/* Just incase if css injection doesn't work */
+$("body, img").attr("style","filter: grayscale(0) !important ; -webkit-filter:grayscale(0) !important");
